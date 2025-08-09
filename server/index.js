3b7db653d17293f3921ce9718a856be6bbc4cb15
TochuGV <tobiasvallejos05@gmail.com>
2025-08-09 01:42:10 -0300

    First commit

---
+++ server/index.js
@@ -0,0 +1,12 @@
+import express from 'express';
+
+const port = 3000;
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('<h1>Hello World!</h1>');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
