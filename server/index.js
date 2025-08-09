1feee3f960afa5b5446b2afd15b959402bf675e8
TochuGV <tobiasvallejos05@gmail.com>
2025-08-09 14:08:35 -0300

    Add real-time message broadcasting

--- server/index.js
+++ server/index.js
@@ -1,12 +1,33 @@
 import express from 'express';
+import logger from 'morgan';
+
+import { Server } from 'socket.io';
+import { createServer } from 'http';
 
 const port = 3000;
 const app = express();
+const server = createServer(app);
+const io = new Server(server);
 
-app.get('/', (req, res) => {
-  res.send('<h1>Hello World!</h1>');
+io.on('connection', (socket) => {
+  console.log('A user connected');
+
+  socket.on('disconnect', () => {
+    console.log('A user disconnected');
+  });
+
+  socket.on('chat message', (msg) => {
+    console.log('Message: ' + msg)
+    socket.broadcast.emit('chat message', msg);
+  });
 });
 
-app.listen(port, () => {
+app.use(logger('dev'));
+
+app.get('/', (req, res) => {
+  res.sendFile(process.cwd() + '/client/index.html');
+});
+
+server.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
