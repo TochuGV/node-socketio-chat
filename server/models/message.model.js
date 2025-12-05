5c30173bb5c7f925c6973134c0b6c4dc1855f733
TochuGV <tobiasvallejos05@gmail.com>
2025-12-05 02:08:27 -0300

    Implement XSS protection and input validation

--- server/models/message.model.js
+++ server/models/message.model.js
@@ -1,9 +1,10 @@
 import mongoose from 'mongoose';
+import sanitizeInput from "../utils/security.util.js";
 
 const messageSchema = new mongoose.Schema({
   userId: { type: String, required: true},
-  username: { type: String, required: true },
-  message: { type: String },
+  username: { type: String, required: true, set: sanitizeInput },
+  message: { type: String, trim: true, set: sanitizeInput },
   audio: { type: Buffer },
   audioType: { type: String },
   timestamp: { type: Date, default: Date.now }
---
+++ server/utils/security.util.js
@@ -0,0 +1,11 @@
+const sanitizeInput = (text) => {
+  if (!text) return text;
+  return text
+    .replace(/&/g, '&amp;')
+    .replace(/</g, '&lt;')
+    .replace(/>/g, '&gt;')
+    .replace(/"/g, '&quot;')
+    .replace(/'/g, '&#039;');
+};
+
+export default sanitizeInput;
